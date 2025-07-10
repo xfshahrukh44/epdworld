e12d2e5e64247a21060c9225f521d64d14ac7095
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-07-10 23:44:23 +0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        $this->call(ConfigSeeder::class);
+        $this->call(\Database\Seeders\ConfigSeeder::class);
     }
 }
