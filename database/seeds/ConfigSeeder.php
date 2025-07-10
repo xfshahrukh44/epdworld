3be5a3cfed3de0c4d665b5ab104c9443574b18ea
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-07-10 23:43:10 +0500

    Update ConfigSeeder.php
    a

--- database/seeds/ConfigSeeder.php
+++ database/seeds/ConfigSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 use Carbon\Carbon;
 
 class ConfigSeeder extends Seeder
