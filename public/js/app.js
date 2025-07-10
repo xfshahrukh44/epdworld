c80d4162a4f5e0001b41e043a8e0ffd6742d791d
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-07-11 00:32:53 +0500

    Comment out Owl Carousel initializations
    The Owl Carousel initializations for .gymSlider and .bannerTwoSlider in app.js have been commented out, disabling these sliders. Additionally, in scripts.blade.php, the Dropify initialization is now wrapped in a check to ensure the plugin is loaded, improving error handling.

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,46 @@
-$('.gymSlider').owlCarousel({
-    "items": 2,
-    "nav": false,
-    "slideSpeed": 300,
-    "dots": true,
-    // "rtl":is_rtl,
-    "paginationSpeed": 400,
-    "navText": ["", ""],
-    "margin": 30,
-    "touchDrag": true,
-    "responsive": {
-        "0": {
-            "items": 1
-        },
-        "480": {
-            "items": 1
-        },
-        "768": {
-            "items": 1
-        },
-        "992": {
-            "items": 2
-        },
-        "1200": {
-            "items": 2
-        }
-    }
-})
+// $('.gymSlider').owlCarousel({
+//     "items": 2,
+//     "nav": false,
+//     "slideSpeed": 300,
+//     "dots": true,
+//     // "rtl":is_rtl,
+//     "paginationSpeed": 400,
+//     "navText": ["", ""],
+//     "margin": 30,
+//     "touchDrag": true,
+//     "responsive": {
+//         "0": {
+//             "items": 1
+//         },
+//         "480": {
+//             "items": 1
+//         },
+//         "768": {
+//             "items": 1
+//         },
+//         "992": {
+//             "items": 2
+//         },
+//         "1200": {
+//             "items": 2
+//         }
+//     }
+// })
 
-$('.bannerTwoSlider').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    dots: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-})
+// $('.bannerTwoSlider').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     dots: false,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 1
+//         },
+//         1000: {
+//             items: 1
+//         }
+//     }
+// })
