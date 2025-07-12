07007a98f5b81089f3c483ed9c971d7bb558e89d
Owais175 <owaisameer014@gmail.com>
2025-07-12 05:23:18 +0500

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -565,8 +565,8 @@
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap-extended.min.css') }}">
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap-extended.min.css') }}"> --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/colors.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/components.min.css') }}">
 </head>
