e707043867b000eb55adf8c85080793989f968a7
Sheikh Huzaifa Farooqi <155949017+SheikhHuzaifa20@users.noreply.github.com>
2025-10-17 22:55:40 +0500

    done

--- resources/views/account/sidebar.blade.php
+++ resources/views/account/sidebar.blade.php
@@ -13,6 +13,9 @@
                 class="{{ request()->route()->getName() == 'myorders' ? 'active' : '' }}"><i
                     class="fa fa-cart-arrow-down"></i>My Orders</a>
 
+            <a href="{{ URL('affiliate_deposit') }}" class="<?php echo (isset($segment[0]) and $segment[0] == 'affiliate_deposit') ? 'active' : ''; ?>"><i class="fa fa-cart-arrow-down"></i>
+                Affiliate Deposit</a>
+
             @if (Auth::user()->is_seller == 1 && Auth::user()->slug)
                 <a href="{{ route('seller-profile', ['slug' => Auth::user()->slug]) }}"
                     class="{{ request()->route()->getName() == 'seller-profile' ? 'active' : '' }}">
@@ -23,9 +26,6 @@
 
         <a href="{{ URL('orders') }}" class="<?php echo (isset($segment[0]) and $segment[0] == 'orders') ? 'active' : ''; ?>"><i class="fa fa-cart-arrow-down"></i> Orders
             History</a>
-
-        <a href="{{ URL('affiliate_deposit') }}" class="<?php echo (isset($segment[0]) and $segment[0] == 'affiliate_deposit') ? 'active' : ''; ?>"><i class="fa fa-cart-arrow-down"></i>
-            Affiliate Deposit</a>
 
         <a href="{{ URL('account-detail') }}" class="<?php echo (isset($segment[0]) and $segment[0] == 'account-detail') ? 'active' : ''; ?>"><i class="fa fa-user"></i> Account Details</a>
 
